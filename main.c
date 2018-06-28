513b449ef0bf3eb7d793ce49db4c90db89eb9060
pfysw <qxqb@163.com>
2018-06-28 11:54:21 +0800

    test

---
+++ main.c
@@ -0,0 +1,47 @@
+/*
+ * main.c
+ *
+ *  Created on: 2017ƒÍ10‘¬29»’
+ *      Author: Administrator
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Bitvec Bitvec;
+//≤‚ ‘≤‚ ‘
+void bittest(void)
+{
+	Bitvec *testBit;
+	testBit = (Bitvec *)sqlite3BitvecCreate(1000000);
+	sqlite3BitvecSet(testBit, 36);
+	printf("hasbit? %d\n",sqlite3BitvecTestNotNull(testBit, 31));
+	printf("hasbit? %d\n",sqlite3BitvecTestNotNull(testBit, 36));
+	sqlite3BitvecDestroy(testBit);
+}
+int main()
+{
+
+	setbuf(stdout, NULL);
+//	int a = 20;
+//	int b = 5;
+//	double c = 100.123456789;
+//	printf("c: %*.*f\n",a,b,c);
+//	printf("c: %*.*e\n",a,b,c);
+//	printf("c: %*.*g\n",a,b,c);
+	int a=123456789;
+	a = a
+			//dsafsf
+			+1;
+	char *b = "dfsf\n";
+	printf("a: %,d\n",a);
+	printf(b);
+	bittest();
+	//SequenceTest();
+	BtreeTest();
+
+	//my_printf("%d %d",a,b);
+	//memtest();
+	//Splitting_A_Linked_List();
+
+}
+
---
+++ s
@@ -0,0 +1,26 @@
+[33mcommit 52a4514192720e7033323342b2bdef6f6df89862[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m)[m
+Author: pfysw <qxqb@163.com>
+Date:   Mon Jun 25 23:42:40 2018 +0800
+
+    Ã·Ωª≤‚ ‘
+    
+    Ã·Ωª≤‚ ‘
+
+[33mcommit 1eedf2b6536857fe13d4f0a814ac4c11f6a5cf1d[m
+Merge: 90dea92 7d64cb2
+Author: pfysw <qxqb@163.com>
+Date:   Mon Jun 25 08:40:37 2018 +0800
+
+    Merge branch 'master' of https://github.com/pfysw/test
+
+[33mcommit 90dea92d4e518d8b5ac932e0dcf37789de6cb3c4[m
+Author: pfysw <qxqb@163.com>
+Date:   Mon Jun 25 06:53:23 2018 +0800
+
+    test commit
+
+[33mcommit 7d64cb2dfa860209ac09ed95ffa544dc1e6c215d[m
+Author: Lijia <qxqb@163.com>
+Date:   Mon Jun 25 06:25:59 2018 +0800
+
+    Initial commit
