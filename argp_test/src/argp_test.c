919b04584b0564805ff3472b770d706c815c6d18
pfysw <qxqb@163.com>
2020-10-08 17:22:06 +0800

    argp test

---
+++ argp_test/src/argp_test.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : argp_test.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int mainll(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
